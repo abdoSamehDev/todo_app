6055a21f47555adbff74d6991a6cbb5c467f3bc9
Abdo Sameh <abdelrahman.sameh.1420@gmail.com>
2022-05-25 19:54:58 +0200

    create database create table open database insert into database get from database

--- lib/layout/layout_screen.dart
+++ lib/layout/layout_screen.dart
@@ -1,7 +1,13 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:todo_app/modules/archived_tasks/archived_tasks_screen.dart';
 import 'package:todo_app/modules/done_tasks/done_tasks_screen.dart';
 import 'package:todo_app/modules/new_tasks/new_tasks_screen.dart';
+import 'package:todo_app/shared/components/components.dart';
+
+import '../shared/components/constants.dart';
 
 class TodoApp extends StatefulWidget {
   const TodoApp({Key? key}) : super(key: key);
@@ -13,6 +19,16 @@ class TodoApp extends StatefulWidget {
 class _TodoAppState extends State<TodoApp> {
 
   int currentIndex = 1;
+  late Database database;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  var formKey = GlobalKey<FormState>();
+  bool isBottomSheetShown = false;
+  IconData fabIcon = Icons.edit;
+  var titleController = TextEditingController();
+  var dateController = TextEditingController();
+  var timeController = TextEditingController();
+  late TimeOfDay? time;
+  late DateTime? date;
 
   List<Widget> screen = [
     NewTaskScreen(),
@@ -20,19 +36,26 @@ class _TodoAppState extends State<TodoApp> {
     ArchivedTaskScreen()
   ];
 
-  List<String> title = [
+  List<String> appBarTitle = [
     'New Tasks',
     'Done Tasks',
     'Archived Tasks'
   ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    databaseCreate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text(
-          title[currentIndex],
-          style: TextStyle(
+          appBarTitle[currentIndex],
+          style: const TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.bold,
             color: Colors.white
@@ -41,29 +64,93 @@ class _TodoAppState extends State<TodoApp> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // try{
-          //   throw('404 Not Found');
-          //   var name = await namePrint();
-          //   print(name);
-          //   throw('404 Not Found');
-          // }
-          // catch(error){
-          //   print('Error ${error.toString()}');
-          // }
-          namePrint().then((value) {
-            print(value);
-            print('Ali');
-            throw('404 Not Found');
-          }).catchError((error){
-            print('Error ${error.toString()}');
-          });
+        onPressed: () async {
+            if(isBottomSheetShown) {
+              if (formKey.currentState!.validate()){
+                await databaseInsert(
+                  title: titleController,
+                  date: date,
+                  time: time
+                );
+                Navigator.pop(context);
+                isBottomSheetShown = false;
+                setState(() {
+                  fabIcon = Icons.edit;
+                });
+                print(titleController.text);
+                print(DateFormat.yMMMd().format(date!));
+                print(time!.format(context));
+              }
+            }
+          else{
+            scaffoldKey.currentState!.showBottomSheet((context) {
+              return Container(
+                color: Colors.grey[100],
+                padding: const EdgeInsets.all(20),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      defaultTextFormField(
+                        label: 'Task Title',
+                        type: TextInputType.text,
+                        controller: titleController,
+                        validateReturn: 'Title Can\'t be empty',
+                        prefix: Icons.title),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      defaultTextFormField(
+                        label: 'Task Date',
+                        type: TextInputType.datetime,
+                        controller: dateController,
+                        validateReturn: 'Date Can\'t be empty',
+                        prefix: Icons.calendar_today_rounded,
+                        onTapFunction: () async {
+                          date = await showDatePicker(context: context, firstDate: DateTime.now(), initialDate: DateTime.now(), lastDate: DateTime.parse('2025-12-31'));
+                          print(DateFormat.yMMMd().format(date!));
+                          dateController.text = DateFormat.yMEd().format(date!);
+                        }
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      defaultTextFormField(
+                          label: 'Task Time',
+                          type: TextInputType.datetime,
+                          controller: timeController,
+                          validateReturn: 'Time Can\'t be empty',
+                          prefix: Icons.watch_later_outlined,
+                          onTapFunction: () async {
+                            time = await showTimePicker(context: context, initialTime: TimeOfDay.now());
+                            print(time!.format(context));
+                            timeController.text = time!.format(context).toString();
+                          }
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+              elevation: 20,
+            ).closed.then((value){
+              isBottomSheetShown = false;
+              setState(() {
+                fabIcon = Icons.edit;
+              });
+            });
+            isBottomSheetShown = true;
+            setState(() {
+              fabIcon = Icons.add;
+            });
+          }
 
         },
-      child: Icon(Icons.add),
+      child: Icon(fabIcon),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
          BottomNavigationBarItem(
              icon: Icon(Icons.menu),
          label: 'New Tasks'
@@ -85,12 +172,70 @@ class _TodoAppState extends State<TodoApp> {
           });
         },
       ),
-      body: screen [currentIndex],
+      body: ConditionalBuilder(
+        condition: tasks.isNotEmpty,
+        builder: (context) => screen [currentIndex],
+        fallback: (context) => Center(child: CircularProgressIndicator()),
+      ),
 
     );
   }
 
-  Future<String> namePrint() async {
-    return 'Abdo Sameh';
+  void databaseCreate() async {
+    database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, version) async{
+        print('database created');
+        try{
+        await database.execute(
+            'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)'
+        );
+        print('table created');}
+        catch(error){
+            print('Error found ${error.toString()}');
+        }
+      },
+      onOpen: (database) async{
+        tasks = await getDataFromDatabase(database);
+          print(tasks);
+        print('database opened');
+      },
+
+    );
+
   }
+
+
+
+  Future <void> databaseInsert({
+    required title,
+    required date,
+    required time,
+}) async {
+    await database.transaction((txn) async {
+      try{
+      int id = await txn.rawInsert('INSERT INTO tasks(title, date, time, status) VALUES("${title.text}", "${DateFormat.yMEd().format(date!)}", "${time!.format(context).toString()}", "new")');
+      print('$id added successfully');
+      }
+      catch(error){
+        print('Error Found ${error.toString()}');
+      }
+    });
+  }
+
+  Future<List<Map>> getDataFromDatabase(database)async{
+    return await database.rawQuery('SELECT * FROM tasks');
+  }
+
+
+  // Future<void> databaseInsert() async {
+  //   database.transaction((txn) async {
+  //           txn.rawInsert('INSERT INTO tasks(title, date, time, status) VALUES("go to gym", "20/5/2022", "10 AM", "new")').then((value) {
+  //             print('${value.toString()} added successfully');
+  //           }).catchError((error){
+  //             print('Error Found ${error.toString()}');
+  //           });
+  //   });
+  // }
 }
---
+++ lib/shared/components/components.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/shared/components/constants.dart';
+Widget defaultButton({
+  double width = double.infinity,
+  Color backgroundColor = Colors.blue,
+  double radius = 10,
+  required void Function()? function,
+  required String text,
+  double fontSize = 25,
+  Color fontColor = Colors.white
+}
+    ) => Container(
+  decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(radius),
+      color: backgroundColor
+  ),
+  width: width,
+  // color: Colors.lightBlue,
+  child: MaterialButton(
+    onPressed: function,
+    child: Text(
+      text.toUpperCase(),
+      style: TextStyle(
+          fontSize: fontSize,
+          color: fontColor
+
+      ),
+    ),
+  ),
+);
+
+Widget defaultTextFormField({
+  bool isPassword = false,
+  required String label,
+  required TextInputType type,
+  required TextEditingController controller,
+  void Function(String)? onSubmitted,
+  required String validateReturn,
+  required IconData prefix,
+  IconData? suffix,
+  void Function()? suffixButtonFunction,
+  void Function()? onTapFunction,
+  bool border = true
+
+}) => TextFormField(
+  validator: (value)
+  {
+    if(value!.isEmpty)
+    {
+      return validateReturn;
+    }
+    return null;
+  },
+  controller: controller,
+  onFieldSubmitted: onSubmitted,
+  keyboardType: TextInputType.visiblePassword,
+  obscureText: isPassword,
+  onTap: onTapFunction,
+  decoration: InputDecoration(
+    labelText: label,
+    prefixIcon: Icon(prefix),
+    suffixIcon: suffix != null ? IconButton(icon: Icon(suffix),
+      onPressed: suffixButtonFunction,
+    ) : null,
+    border: border != true ? null : const OutlineInputBorder(),
+  ),
+);
+
+Widget buildTaskItem(Map model) => Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          child: Text(
+            '${model['time']}',
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${model['title']}',
+              style: TextStyle(
+                fontSize: 25,
+                // color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${model['date']}',
+              style: TextStyle(
+                  color: Colors.grey
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
